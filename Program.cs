32c50b242818e12e5fc82d877092859ac1dbe84a
«Roman <research777@mail.ru>
2022-12-26 17:23:30 +0300

    C#Prog_Fnl

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿string [] mainArray = new string [6] { "Hello", "SPb", "2", "World", "etc", "Bye" };
+string [] newArray = new string[mainArray.Length];
+
+void FnlArrayElements (string [] mainArray, string [] newArray)
+{   
+    int count = 0;
+    for (int i = 0; i < mainArray.Length; i++)
+        if (mainArray[i].Length <=3)
+        {
+            newArray[count] = mainArray[i];
+            count++;
+        }
+}
+
+void ShowArray (string [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write ($"{array[i]} ");
+    Console.WriteLine();
+}
+
+FnlArrayElements(mainArray, newArray);
+ShowArray(newArray);
+
